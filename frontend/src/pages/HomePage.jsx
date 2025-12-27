6ac3a127d0b6e3322216ec600572fb640db56156
SaiKriCius <jatin6410kumar@gmail.com>
2025-12-27 11:51:17 +0530

    a

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const categories = [
 	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
 	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
 	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
-	{ href: "/bags", name: "Cosmetics", imageUrl: "/bags.jpg" },
+	{ href: "/cosmetics", name: "Cosmetics", imageUrl: "/cosmetics.jpg" },
 ];
 
 const HomePage = () => {
