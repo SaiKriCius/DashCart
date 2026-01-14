06045d90576dcb03bb8b0912118c5e887c99d0d7
SaiKriCius <jatin6410kumar@gmail.com>
2026-01-14 18:28:39 +0530

    link change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.DEV
 		? "http://localhost:5000/api"
-		: "https://dashcart-production.up.railway.app/api",
+		: "https://dashcart-ihxmz.sevalla.app/api",
 	withCredentials: true,
 });
 
