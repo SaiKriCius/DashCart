9a2380598f94f78410212d91dd2c7424fd9f8890
SaiKriCius <jatin6410kumar@gmail.com>
2026-03-22 13:25:23 +0530

    upstash changes

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,12 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+  tls: {},
+});
+
+redis.on("error", (err) => {
+  console.log("Redis error:", err.message);
+});
